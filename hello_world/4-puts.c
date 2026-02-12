95560016fda23a772ed347de1edead081b72db5d
Friasru <albertofriasru@gmail.com>
2026-02-12 16:46:01 -0400

    updated file

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
 /**
-* print followed by a new lane
+* main - prints a string followed by a new line
+*
+* Description: Uses puts to print the exact string
+* "\"Programming is like building a multilingual puzzle\""
+*
+* Return: Always 0
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
