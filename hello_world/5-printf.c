1bca8a0ee64779623ab1ddb35823e67cded1a70e
Friasru <albertofriasru@gmail.com>
2026-02-12 17:00:08 -0400

    print exactly the text

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints a string followed by a new line
+*
+* return: Always 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
