4ed4e74a7101b21fbd5f438d886202ab6e79aa59
Friasru <albertofriasru@gmail.com>
2026-02-12 16:31:04 -0400

    updated file fixed

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
 /**
  * print followed by a new lane
  */
- 
  int main(void)
  {
      puts("\"Programming is like building a multilingual puzzle");
-     return 0;
+     return (0);
  }
