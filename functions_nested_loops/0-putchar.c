b68b6ee39fa9191540378379d4085181e7090b08
Friasru <albertofriasru@gmail.com>
2026-02-18 09:15:59 -0800

    updated script

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-/**
+/**\
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-printf("-putchar.c\n");
+putchar ('_');
+putchar ('p');
+putchar ('u');
+putchar ('t');
+putchar ('c');
+putchar ('h');
+putchar ('a');
+putchar ('r');
+putchar ('\n');
 
 return (0);
 }
