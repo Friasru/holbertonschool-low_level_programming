93f938c1fbe3509ef499313bd79ad7406195ccab
Friasru <albertofriasru@gmail.com>
2026-02-24 10:03:13 -0800

    create a sequence

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,7 +2,10 @@
 
 /**
 * main - write a function that fills with a constant byte
-*
+* @s: represents the memory you want to modify
+* @b: is the value or "byte" you want to put in memory
+* @n: is how many bytes you change
+* _memset: fill the memory and write the same byte repeatedly
 * Result: a function that fills memory with a constant file
 */
 char *_memset(char *s, char b, unsigned int n)
