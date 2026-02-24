b81ed9d50802d54c4f5bafce8bb9b1d2eba2fef5
Friasru <albertofriasru@gmail.com>
2026-02-24 10:41:19 -0800

    change destination

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _memcpy - copy memory area
+* @dest: is the destination
+* @src: is the source
+* @n: moves n bytes from src to dest
+*
+* Return: returns a pointer to destination
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    unsigned int i;
+
+    for (i = 0; i < n; i++)
+    {
+        dest[i] = src[i];
+    }
+    return dest;
+}
