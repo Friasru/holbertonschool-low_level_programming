da801ea69ed0faff47ad7cbbc93ab8623aa97232
Friasru <albertofriasru@gmail.com>
2026-03-10 13:20:53 -0700

    updated betty documentation

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_interator - executes a function given as parameter
+ * array_iterator - executes a function given as parameter
  * @array: array to iterate over
  * @size: size of the array
  * @action: pointer to the function to execute
