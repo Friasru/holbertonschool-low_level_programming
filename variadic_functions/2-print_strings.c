27e27a05bc716f898c62c9ca4228fe2ad69712d5
Friasru <albertofriasru@gmail.com>
2026-03-16 07:28:12 -0700

    fixed ERROR: space required before the open parenthesis

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 
 	va_start(ap, n);
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(ap, char *);
 		if (str == NULL)
