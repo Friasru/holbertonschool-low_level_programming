5b9aa28f9337ca942b3a1f1fb3b55f76fa0c4875
Friasru <albertofriasru@gmail.com>
2026-03-16 14:54:52 -0700

    This script prints anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments passed to the function
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	const char *f;
+	char *str;
+
+	va_start(ap, format);
+	f = format;
+
+	while (*f)
+	{
+		switch (*f)
+		{
+		case 'c':
+			printf("%c", va_arg(ap, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(ap, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ap, int));
+			break;
+		case 's':
+			str = va_arg(ap, char *);
+			str && printf("%s", str);
+			!str && printf ("(nil)");
+			break;
+		}
+		f++;
+
+		while (*f && *f != 'c' && *f != 'i' && *f != 'f' && *f != 's')
+			f++;
+
+		if (*f)
+			printf (", ");
+	}
+	va_end(ap);
+	printf("\n");
+}
