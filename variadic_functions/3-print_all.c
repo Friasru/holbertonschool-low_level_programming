8d6d3ff7b7bf9ae167e419e0ac4345cd7bbec7ec
Friasru <albertofriasru@gmail.com>
2026-03-17 14:01:59 -0700

    fixed betty errors 38 warnings

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -16,41 +16,41 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-    int i, first;
-    char *str;
+	int i, first;
+	char *str;
 
-    va_start(args, format);
-    i = 0;
-    first = 0;
-    while (format && format[i])
-    {
-        if ((first != 0) && (format[i] == 'c' || format[i] == 'i' 
-            || format[i] == 'f' || format[i] == 's'))
-            printf(", ");
-        switch (format[i])
-        {
-            case 'c':
-                printf("%c", va_arg(args, int));
-                first = 1;
-                break;
-            case 'i':
-                printf("%d", va_arg(args, int));
-                first = 1;
-                break;
-            case 'f':
-                printf("%f", va_arg(args, double));
-                first = 1;
-                break;
-            case 's':
-                str = va_arg(args, char *);
-                if (str)
-                    printf("%s", str);
-                printf("(nil)" + 5 * (str != NULL));
-                first = 1;
-                break;
-        }
-        i++;
-    }
-    va_end(args);
-    printf("\n");
+	va_start(args, format);
+	i = 0;
+	first = 0;
+	while (format && format[i])
+	{
+		if ((first != 0) && (format[i] == 'c' || format[i] == 'i'
+			|| format[i] == 'f' || format[i] == 's'))
+			printf(", ");
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				first = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				first = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				first = 1;
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (str)
+					printf("%s", str);
+				printf("(nil)" + 5 * (str != NULL));
+				first = 1;
+				break;
+		}
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }
