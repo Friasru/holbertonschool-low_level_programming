752b94a0af2977a38b7ee5f098dcffc25553cba0
Friasru <albertofriasru@gmail.com>
2026-03-18 14:46:52 -0700

    added script in line 12

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,11 +9,14 @@ void jack_bauer(void)
 
 	for (h = 0; h < 24; h++)
 	{
+		for (m = 0; m < 60; m++)
+		{
 		_putchar('0' + h / 10);
 		_putchar('0' + h % 10);
 		_putchar(':');
 		_putchar('0' + m / 10);
 		_putchar('0' + m % 10);
 		_putchar('\n');
+		}
 	}
 }
