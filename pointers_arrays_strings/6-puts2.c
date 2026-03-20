11ab42dd5ccb3d2274bee7676eab8f44499a4017
Friasru <albertofriasru@gmail.com>
2026-03-20 14:19:03 -0700

    fixed code

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,13 @@
 void puts2(char *str)
 {
 	int i;
+	int len;
 
-	for (i = 0; str[i]; i += 2)
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
 }
