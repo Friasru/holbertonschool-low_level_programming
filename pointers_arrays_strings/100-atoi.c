e49d08b95c040724264bae6f61a2c8091ddc534c
Friasru <albertofriasru@gmail.com>
2026-03-24 12:00:07 -0700

    Fix: prevent overflow before multiplication

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -28,18 +28,25 @@ int _atoi(char *s)
 		{
 		}
 		else if (*s >= '0' && *s <= '9')
+		{
+			started = 1;
+			digit = *s - '0';
+			if (result > 214748364)
 			{
-				started = 1;
-				digit = *s - '0';
-				if (result > 214748364 || (result == 214748364 && digit > 8))
-				{
-					if (sign == 1)
-						return (214748364);
-					else
-						return (-214748364);
-				}
-				result = result * 10 + digit;
+				if (sign == 1)
+					return (2147483647);
+				else
+					return (-2147483648);
 			}
+			if (result == 214748364 && digit > 7)
+			{
+				if (sign == 1)
+					return (2147483647);
+				else
+					return (-2147483648);
+			}
+			result = result * 10 + digit;
+		}
 		else if (started == 1)
 		{
 			break;
