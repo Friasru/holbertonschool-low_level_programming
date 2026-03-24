676c5de42bb6061aa0e462ac42d3dc50e0991280
Friasru <albertofriasru@gmail.com>
2026-03-24 12:01:26 -0700

    Reduced function size

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -21,36 +21,23 @@ int _atoi(char *s)
 	while (*s)
 	{
 		if (*s == '-' && started == 0)
-		{
 			sign *= -1;
-		}
 		else if (*s == '+' && started == 0)
-		{
-		}
+			;
 		else if (*s >= '0' && *s <= '9')
 		{
 			started = 1;
 			digit = *s - '0';
-			if (result > 214748364)
+			if (result > 214748364 || (result == 214748364 && digit > 7))
 			{
 				if (sign == 1)
 					return (2147483647);
-				else
-					return (-2147483648);
-			}
-			if (result == 214748364 && digit > 7)
-			{
-				if (sign == 1)
-					return (2147483647);
-				else
-					return (-2147483648);
+				return (-2147483648);
 			}
 			result = result * 10 + digit;
 		}
 		else if (started == 1)
-		{
 			break;
-		}
 		s++;
 	}
 	return (sign * result);
