20c1bccd01b517165e2f93dfa44a6918ff72da5d
Friasru <albertofriasru@gmail.com>
2026-03-25 09:40:23 -0700

    fix: use i variable for spaces

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -19,9 +19,9 @@ void print_triangle(int size)
 
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - i; j++)
 			_putchar(' ');
-		for (j = 0; j < 1; j++)
+		for (j = 0; j < i; j++)
 			_putchar('#');
 		_putchar('\n');
 	}
