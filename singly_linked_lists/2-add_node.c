764e93494b6dd0131124284881351e41f1d63b04
Friasru <albertofriasru@gmail.com>
2026-03-25 13:04:05 -0700

    implement function to insert nodes at list beginning

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - adds a new node at the beggining of a list_t
+ * @h: pointer to the head of the list
+ * @str: string to be stored in new node
+ *
+ * Return: The address of the new element, or NULL if failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len;
+
+	if (str == NULL)
+		return (NULL);
+	
+	new = malloc(sizeof(list_t));
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	len = 0;
+	while (str[len])
+		len++;
+	
+	new->len = len;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
