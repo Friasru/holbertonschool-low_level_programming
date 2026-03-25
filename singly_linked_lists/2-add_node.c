51d114da1254f3847fa8a0f29ce30670078175dc
Friasru <albertofriasru@gmail.com>
2026-03-25 13:11:34 -0700

    fix: strlen function updated

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,17 +16,19 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (str == NULL)
 		return (NULL);
-	
+
 	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	return (NULL);
+
+	new->str = strdup(str);
 	if (new->str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	len = 0;
-	while (str[len])
-		len++;
-	
+
+	len = strlen(str);
 	new->len = len;
 	new->next = *head;
 	*head = new;
