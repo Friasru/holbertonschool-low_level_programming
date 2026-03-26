474688ed5b664ccc4d06dbbf3c9ad7669a66444f
Friasru <albertofriasru@gmail.com>
2026-03-26 13:32:31 -0700

    fix: line 32-33 missing implementation

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -29,5 +29,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	while (current->next != NULL)
 	current = current->next;
 
+	current->next = new;
+	new->prev = current;
+
 	return (new);
 }
