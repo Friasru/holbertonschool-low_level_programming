1ac4606a1faa687eb19cb3e93066f0b4077439be
Friasru <albertofriasru@gmail.com>
2026-03-27 08:50:20 -0700

    fix: betty coding style space required before the open parenthesis

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 	content_len = 0;
-	while(text_content[content_len])
+	while (text_content[content_len])
 	content_len++;
 
 	bytes_written = write(fd, text_content, content_len);
