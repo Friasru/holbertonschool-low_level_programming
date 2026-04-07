c06f1828c9842d76d1dbea9c24a24a31c788aba8
Friasru <albertofriasru@gmail.com>
2026-04-07 14:46:36 -0700

    removed extra indentation on line 21

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (ht == NULL)
 		return (NULL);
 
-		ht->array = malloc(size * sizeof(hash_node_t *));
+	ht->array = malloc(size * sizeof(hash_node_t *));
 		if (ht->array == NULL)
 	{
 		free(ht);
@@ -26,7 +26,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	for (i = 0; i < size; i++)
-	if (ht->array == NULL)
+		ht->array[i] = NULL;
 
 	ht->size = size;
 
