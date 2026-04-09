994515eb3ed6b1cd9df843c0e6fe59dbba485d8e
Friasru <albertofriasru@gmail.com>
2026-04-09 13:08:48 -0700

    moved add_node above hash_table_set so the compiler sees it before its called

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,39 @@
 #include "hash_tables.h"
 
 /**
+ * add_node - creates and adds a new node at the beginning of the list
+ * @ht: the hash table
+ * @idx: the index in the array
+ * @key: the key
+ * @value_dup: the already duplicated value
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int add_node(hash_table_t *ht, unsigned long int idx,
+	const char *key, char *value_dup)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+	{
+		free(value_dup);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(value_dup);
+		free(new_node);
+		return (0);
+	}
+	new_node->value = value_dup;
+	new_node->next = ht->array[idx];
+	ht->array[idx] = new_node;
+	return (1);
+}
+
+/**
  * hash_table_set - adds an element to the hash table
  * @ht: the hash table to add or update the key/value to
  * @key: the key (cannot be an empty string)
@@ -34,37 +67,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		tmp = tmp->next;
 	}
 	return (add_node(ht, idx, key, value_dup));
-}
-
-/**
- * add_node - creates and adds a new node at the beginning of the list
- * @ht: the hash table
- * @idx: the index in the array
- * @key: the key
- * @value_dup: the already duplicated value
- *
- * Return: 1 if it succeeded, 0 otherwise
- */
-int add_node(hash_table_t *ht, unsigned long int idx,
-	const char *key, char *value_dup)
-{
-	hash_node_t *new_node;
-
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
-	{
-		free(value_dup);
-		return (0);
-	}
-	new_node->key = strdup(key);
-	if (!new_node->key)
-	{
-		free(value_dup);
-		free(new_node);
-		return (0);
-	}
-	new_node->value = value_dup;
-	new_node->next = ht->array[idx];
-	ht->array[idx] = new_node;
-	return (1);
 }
