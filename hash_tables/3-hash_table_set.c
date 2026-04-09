20e083deae97d17a3668ff4feccbd14c2f473088
Friasru <albertofriasru@gmail.com>
2026-04-09 12:59:00 -0700

    split the node logit into a separate add_node

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *tmp;
+	hash_node_t *tmp;
 	char *value_dup;
 	unsigned long int idx;
 
@@ -21,6 +21,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	value_dup = strdup(value);
 	if (!value_dup)
 		return (0);
+
 	tmp = ht->array[idx];
 	while (tmp)
 	{
@@ -32,13 +33,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		tmp = tmp->next;
 	}
+	return (add_node(ht, idx, key, value_dup));
+}
+
+/**
+ * add_node - creates and adds a new node at the beginning of the list
+ * @ht: the hash table
+ * @idx: the index in the array
+ * @key: the key
+ * @value_dup: the already duplicated value
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int add_node(hash_table_t *ht, unsigned long int idx,
+	const char *key, char *value_dup)
+{
+	hash_node_t *new_node;
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 	{
 		free(value_dup);
 		return (0);
 	}
-
 	new_node->key = strdup(key);
 	if (!new_node->key)
 	{
@@ -46,10 +63,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	new_node->value = value_dup;
 	new_node->next = ht->array[idx];
 	ht->array[idx] = new_node;
-
 	return (1);
 }
