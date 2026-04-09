6e7598ea13ddeb4eab8be8d2d541d8adbdfac33f
Friasru <albertofriasru@gmail.com>
2026-04-09 11:41:43 -0700

    previous version of the code

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,34 +10,44 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node, *new;
-	unsigned long int index;
-	char *val_copy, *key_copy;
+    hash_node_t *new_node;
+    unsigned long int index;
+    char *value_copy;
+    char *key_copy;
 
-	if (!ht || !key || !*key)
-		return (0);
-	val_copy = malloc(strlen(value) + 1);
-	if (!val_copy)
-		return (0);
-	strcpy(val_copy, value);
-	index = key_index((unsigned char *)key, ht->size);
-	node = ht->array[index];
-	while (node)
-	{
-		if (strcmp(node->key, key))
-			return(free(node->value), node->value = val_copy, 1);
-		new = node->next;
-	}
-	key_copy = malloc(strlen(key) + 1);
-	if (!key_copy)
-		return (free(val_copy), 0);
-	strcpy(new->key, key);
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
-		return (free(val_copy), free(key_copy), 0);
-	new->key = key_copy;
-	new->value = val_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
-	return (1);
+    if (ht == NULL || key == NULL || *key == '\0')
+        return (0);
+
+    value_copy = malloc(strlen(value) + 1);
+    if (value_copy == NULL)
+        return (0);
+    strcpy(value_copy, value);
+
+    index = key_index((unsigned char *)key, ht->size);
+
+    if (_find_and_update(ht->array[index], key, value_copy))
+        return (1);
+
+    key_copy = malloc(strlen(key) + 1);
+    if (key_copy == NULL)
+    {
+        free(value_copy);
+        return (0);
+    }
+    strcpy(key_copy, key);
+
+    new_node = malloc(sizeof(hash_node_t));
+    if (new_node == NULL)
+    {
+        free(value_copy);
+        free(key_copy);
+        return (0);
+    }
+
+    new_node->key = key_copy;
+    new_node->value = value_copy;
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
+
+    return (1);
 }
