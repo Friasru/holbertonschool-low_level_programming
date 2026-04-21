e6979a5a012ea5daa4e918c3792c9087122d338a
Friasru <albertofriasru@gmail.com>
2026-04-21 11:46:11 -0700

    splted long lines and fix indentation

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -4,30 +4,31 @@
  * cap_string - capitalizes all words of a string
  * @str: string to capitalize
  *
- * return: pointer to the modified string
+ * Return: pointer to the modified string
  */
 char *cap_string(char *str)
 {
-	int i = 0;
-	int is_sep;
+    int i = 0;
+    int is_sep;
 
-	while (str[i] != '\0')
-	{
-		is_sep = 0;
+    while (str[i] != '\0')
+    {
+        is_sep = 0;
 
-		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
-			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
-			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
-			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
-			{
-				is_sep = 1;
-			}
+        if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+            str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';'
+            || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?'
+            || str[i - 1] == '"' || str[i - 1] == '('
+            || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+        {
+            is_sep = 1;
+        }
 
-			if (is_sep && str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-			}
-			i++;
-	}
-	return (str);
+        if (is_sep && str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;
+
+        i++;
+    }
+
+    return (str);
 }
