904f212bfac4eff42195ec43627a2088fb0a1f8c
Friasru <albertofriasru@gmail.com>
2026-04-21 12:28:50 -0700

    fixed no spaces at the start of the line

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,27 +8,27 @@
  */
 char *cap_string(char *str)
 {
-    int i = 0;
-    int is_sep;
+	int i = 0;
+	int is_sep;
 
-    while (str[i] != '\0')
-    {
-        is_sep = 0;
+	while (str[i] != '\0')
+	{
+		is_sep = 0;
 
-        if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
-            str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';'
-            || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?'
-            || str[i - 1] == '"' || str[i - 1] == '('
-            || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
-        {
-            is_sep = 1;
-        }
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+			str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';'
+			|| str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?'
+			|| str[i - 1] == '"' || str[i - 1] == '('
+			|| str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+		{
+			is_sep = 1;
+		}
 
-        if (is_sep && str[i] >= 'a' && str[i] <= 'z')
-            str[i] = str[i] - 32;
+		if (is_sep && str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
 
-        i++;
-    }
+		i++;
+	}
 
-    return (str);
+	return (str);
 }
