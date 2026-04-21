5b8f2091a4729415e117ca06acf84dde39b671b1
Friasru <albertofriasru@gmail.com>
2026-04-21 11:39:21 -0700

    function capitalizes all words separated by space, tabs

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to capitalize
+ *
+ * return: pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int is_sep;
+
+	while (str[i] != '\0')
+	{
+		is_sep = 0;
+
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+			{
+				is_sep = 1;
+			}
+
+			if (is_sep && str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+			else if (!is_sep && str[i] >= 'A' && str[i] <= 'Z')
+			{
+				str[i] = str[i] + 32;
+			}
+
+			i++;
+	}
+	return (str);
+}
