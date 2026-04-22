599e27b9e5a29975f1c4c351a6b41f6bed19071a
Friasru <albertofriasru@gmail.com>
2026-04-22 11:57:53 -0700

    added helper functions to the file

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,41 @@
 #include "dog.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * _strcpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: dog name
  * @age: dog age
